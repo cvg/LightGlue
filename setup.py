bbf3cede9819a7f29eb78da5d53d0ffb6ca3c07b
Phil26AT <phil.lindenberger@gmail.com>
2023-07-11 12:12:50 +0200

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ description = ['LightGlue']
 
 with open(str(Path(__file__).parent / 'README.md'), 'r', encoding='utf-8') as f:
     readme = f.read()
-
-extra_dependencies = ['torch', 'kornia', 'numpy', 'einops']
+with open(str(Path(__file__).parent / 'requirements.txt'), 'r') as f:
+    dependencies = f.read().split('\n')
 
 setup(
     name='lightglue',
     version='0.0',
     packages=['lightglue'],
     python_requires='>=3.6',
-    extras_require={'extra': extra_dependencies},
+    install_requires=dependencies,
     author='Philipp Lindenberger, Paul-Edouard Sarlin',
     description=description,
     long_description=readme,
