c28c5c5705751bd8780b376f2df6eb2bf703303c
Spike Xiong <abc62425679@gmail.com>
2023-10-07 10:10:02 +0800

    Update lightglue.py

--- lightglue/lightglue.py
+++ lightglue/lightglue.py
@@ -290,7 +290,7 @@ class MatchAssignment(nn.Module):
 
 
 def filter_matches(scores: torch.Tensor, th: float):
-    """ obtain matches from a log assignment matrix [Bx M+1 x N+1]"""
+    """obtain matches from a log assignment matrix [Bx M+1 x N+1]"""
     assert scores.shape.__len__() == 3
     max0, max1 = scores[:, :-1, :-1].max(2), scores[:, :-1, :-1].max(1)
     m0, m1 = max0.indices, max1.indices
